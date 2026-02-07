045d1ca2cad78e48f32ca8eb86f6dec4f4c5bd33
mariana-garcia844 <mariana20007g@gmail.com>
2026-02-07 15:21:33 -0600

    Added Firebase Source
    Added firebase js file and script tag to the top of every html page.

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1bLdkuYRQuCrG5Fpk_qlmXQA7Z964Bc0",
+    authDomain: "portfolio-backend-0791.firebaseapp.com",
+    projectId: "portfolio-backend-0791",
+    storageBucket: "portfolio-backend-0791.firebasestorage.app",
+    messagingSenderId: "46489525154",
+    appId: "1:46489525154:web:f7b72869b0989bca6830fb",
+    measurementId: "G-3YNGWHFF6Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Export the app so other files can use it later
+export { app };
